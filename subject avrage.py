67339aec8d9e35c94a8289dce5635d2f6704c388
khevnaKushwaha <165312875+khevnaKushwaha@users.noreply.github.com>
2024-03-29 20:41:27 +0530

    Add files via upload

---
+++ subject avrage.py
@@ -0,0 +1,11 @@
+name=input("enter the name")
+age= int(input("enter the age"))
+
+maths=int(input("enter maths marks out of 100"))
+english=int(input("enter english marks out of 100"))
+hindi=int(input("enter hindi marks out of 100"))
+sst=int(input("enter s.s.t marks out of 100"))
+computer=int(input("enter computer marks out of 100"))
+
+avrage=(maths+english+hindi+sst+computer)/5
+print("the avrage is ",avrage)
